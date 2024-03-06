b5b7b2296fa3ba39789212546a6d3cf84efc7a99
Trishane <136679393+trishanetrx@users.noreply.github.com>
2024-03-06 16:38:22 +0530

    cleaned.java

--- BadCodeExample.java
+++ BadCodeExample.java
@@ -1,19 +1,1 @@
-// TestProject.java
-public class TestProject {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Test Project!");
-        
-        // Create an instance of TestClass
-        TestClass testInstance = new TestClass();
-        
-        // Call the method from TestClass
-        testInstance.printMessage();
-    }
-}
 
-// TestClass.java
-public class TestClass {
-    public void printMessage() {
-        System.out.println("This is a message from TestClass.");
-    }
-}
